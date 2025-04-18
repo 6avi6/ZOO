fbe559ba3a7d449c1335f319e74351d0b051ec1c
Ema <emajure27@gmail.com>
2025-04-18 20:27:46 +0200

    fix: typo 5 instead of 6

--- zooBackend/src/main/java/com/polsl/tab/zoobackend/config/JwtUtil.java
+++ zooBackend/src/main/java/com/polsl/tab/zoobackend/config/JwtUtil.java
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class JwtUtil {
     private static final String SECRET_KEY = "3Fk9bH2nV6D7Ml7H9QzPjQogS2nB8tVWyH4E0YYvD2g=";
-    private static final long ACCESS_TOKEN_EXPIRATION = 1000 * 50 * 15; // 15 minutes
+    private static final long ACCESS_TOKEN_EXPIRATION = 1000 * 60 * 15; // 15 minutes
     private static final SecureRandom secureRandom = new SecureRandom();
     private static final int TOKEN_LENGTH = 32; // Length in bytes (256 bits)
 

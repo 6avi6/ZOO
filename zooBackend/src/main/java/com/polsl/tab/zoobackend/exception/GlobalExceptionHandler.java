f8af3cb7838c3c25d6af4bf6db9584b21c50b874
jemek27 <emajure27@gmail.com>
2025-05-22 01:24:30 +0200

    refactor

--- zooBackend/src/main/java/com/polsl/tab/zoobackend/exception/GlobalExceptionHandler.java
+++ zooBackend/src/main/java/com/polsl/tab/zoobackend/exception/GlobalExceptionHandler.java
@@ -114,10 +114,4 @@ public class GlobalExceptionHandler {
         );
         return new ResponseEntity<>(body, status);
     }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalArgument(IllegalArgumentException ex) {
-        ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Bad Request", ex.getMessage());
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
 }
